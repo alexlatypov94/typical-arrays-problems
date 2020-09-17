e594773485669be48defd26b98864c9a22a7b7a6
Aliaksei <alex.latypov94@gmail.com>
2020-09-17 18:47:13 +0300

    feat: add task solution typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0 
+} else {
+  return Math.min(...array)
+}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0 
+} else {
+  return Math.max(...array)
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+    return 0
+} else {
+  let sum = array.reduce((accum, curr) => {
+    return accum + curr
+  });
+  return sum / array.length
+}
 }
